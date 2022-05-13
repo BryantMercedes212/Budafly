465dd6a0b516ad1eb360e19bd0bfc8e66882818b
Bryant Mercedes <bryantmercedescanela@gmail.com>
2022-05-13 15:59:59 -0400

    chnaged dbconfig

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -1,14 +1,24 @@
-const pgp = require('pg-promise')();
-require('dotenv').config();
+const pgp = require("pg-promise")();
+require("dotenv").config();
 
-const { PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
-
-const cn = {
-    host: PG_HOST,
-    port: PG_PORT,
-    database: PG_DATABASE,
-    user: PG_USER, 
-}
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+      // password: PG_PASSWORD,
+    };
 
 const db = pgp(cn);
 
