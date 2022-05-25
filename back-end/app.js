64671966ae454198827e3ad2aa851d9add6e0b70
Bryant Mercedes <bryantmercedescanela@gmail.com>
2022-05-25 16:32:52 -0400

    login

--- back-end/app.js
+++ back-end/app.js
@@ -17,6 +17,10 @@ app.get("/", (request, response) => {
 const productsController = require("./controllers/productsController.js");
 app.use("/products", productsController);
 
+//  Login ROUTES
+const login = require("./controllers/login.js");
+app.use("/login", login);
+
 // 404 Page
 app.get("*", (request, response) => {
   response.status(404).send("Page not found");
