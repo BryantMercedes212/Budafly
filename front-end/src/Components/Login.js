83565ff5bb5d3ff57159674fd020ab3fd192a6cd
Bryant Mercedes <bryantmercedescanela@gmail.com>
2022-05-31 11:35:58 -0400

    updated logIN and SearchBar

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -1,7 +1,29 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [account, setAccount] = useState({});
+
+  const fetchProduct = async () => {
+    try {
+      const res = await axios.get(`http://localhost:3333/login/${email}`);
+      setAccount(res.data);
+    } catch (error) {
+      console.log(error);
+      setAccount({});
+    }
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="text-center m-5-auto">
       <h2>Log In</h2>
@@ -9,12 +31,26 @@ export default function LogIn() {
         <div>
           <label for="email">Email Address</label>
           <br />
-          <input type="email" id="email" name="email" required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+            required
+          />
         </div>
         <div>
           <label>Password</label>
           <br />
-          <input type="password" id="password" name="password" required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            required
+            onChange={handlePassword}
+          />
         </div>
         <div>
           <button id="submitButton" type="submit">
