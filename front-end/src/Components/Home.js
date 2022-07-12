605ada2d3d93eb5e52cbca10d96bd2985ce94cb9
Bryant Mercedes <bryantmercedescanela@gmail.com>
2022-07-12 16:25:17 -0400

    made Featured Products dinamic under sigleProductpage and removed some extra div

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -23,38 +23,36 @@ const Home = ({ addItem }) => {
   const allProducts = products.map((product, i) => {
     return (
       <div class="column is-4">
-        <div class="card mt-4 py-3">
-          <div class="card-image pr-3 pl-4 has-background-primary ">
-            <figure class="image is-128x128 ">
-              <img src={product.image} alt={product.description} />
-            </figure>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-content">
-                  <p class="title is-4">{product.name}</p>
-                  <p class="subtitle is-6">${product.price}</p>
-                </div>
-              </div>
-
+        <div class="card-image pr-3 pl-4 has-background-primary ">
+          <figure class="image is-128x128 ">
+            <img src={product.image} alt={product.description} />
+          </figure>
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
               <div class="media-content">
-                <Link className="item-link" to={`/products/${product.id}`}>
-                  {" "}
-                  More details
-                </Link>
-                <br></br>
-                <footer class="card-footer">
-                  <div class="buttons">
-                    <button
-                      class="button is-primary"
-                      onClick={() => addItem(product)}
-                    >
-                      <strong>Add To Cart</strong>
-                    </button>
-                  </div>
-                </footer>
+                <p class="title is-4">{product.name}</p>
+                <p class="subtitle is-6">${product.price}</p>
               </div>
+            </div>
+
+            <div class="media-content">
+              <Link className="item-link" to={`/products/${product.id}`}>
+                {" "}
+                More details
+              </Link>
+              <br></br>
+              <footer class="card-footer">
+                <div class="buttons">
+                  <button
+                    class="button is-primary"
+                    onClick={() => addItem(product)}
+                  >
+                    <strong>Add To Cart</strong>
+                  </button>
+                </div>
+              </footer>
             </div>
           </div>
         </div>
--- front-end/src/Components/singleProduct.js
+++ front-end/src/Components/singleProduct.js
@@ -7,6 +7,14 @@ const Product = ({ addItem }) => {
   const [product, setProduct] = useState([]);
   const { id } = useParams();
   const URL = process.env.REACT_APP_API_URL;
+  const [sellerProduct, setSellerProduct] = useState([]);
+  const randomNumbers = [];
+  let i = 0;
+
+  while (i < 4) {
+    randomNumbers.push(Math.floor(Math.random() * 40));
+    i++;
+  }
 
   const fetchProduct = async () => {
     try {
@@ -17,14 +25,67 @@ const Product = ({ addItem }) => {
       setProduct([]);
     }
   };
-  const tryning = "trying";
+
+  const fetchSellersProducts = async () => {
+    try {
+      const res = await axios.get(`${URL}/products/`);
+      setSellerProduct(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+      setSellerProduct([]);
+    }
+  };
 
   useEffect(() => {
     fetchProduct();
   }, []);
 
-  console.log(product);
+  useEffect(() => {
+    fetchSellersProducts();
+  }, []);
 
+  const sellerFeaturedProducts = sellerProduct.map((product) => {
+    return (
+      <div class="column is-4">
+        <div class="card-image pr-3 pl-4 has-background-primary ">
+          <figure class="image is-128x128 ">
+            <img src={product.image} alt={product.description} />
+          </figure>
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">{product.name}</p>
+                <p class="subtitle is-6">${product.price}</p>
+              </div>
+            </div>
+
+            <div class="media-content">
+              <Link className="item-link" to={`/products/${product.id}`}>
+                {" "}
+                More details
+              </Link>
+              <br></br>
+              <footer class="card-footer">
+                <div class="buttons">
+                  <button
+                    class="button is-primary"
+                    onClick={() => addItem(product)}
+                  >
+                    <strong>Add To Cart</strong>
+                  </button>
+                </div>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  console.log(sellerProduct);
   return (
     <div class="start">
       <div class="section">
@@ -64,7 +125,7 @@ const Product = ({ addItem }) => {
                   </div>
                   <div class="message-body">
                     <p>
-                      <strong>Price Per 14 Grams ${product.price}</strong>{" "}
+                      <strong>Price ${product.price}</strong>{" "}
                     </p>
                     <div class="has-text-centered">
                       <button
@@ -96,74 +157,9 @@ const Product = ({ addItem }) => {
         <h3 class="title has-text-centered is-size-4">Featured Products</h3>
         <div class="container">
           <div class="columns is-variable">
-            <div class="column">
-              <div class="card has-background-primary">
-                <div class="card-image has-text-centered px-6">
-                  <img
-                    src="https://i1.wp.com/www.gotweedonline.com/wp-content/uploads/2020/05/1589188622250.jpg?fit=1005%2C1005&ssl=1"
-                    alt=" "
-                  />
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <p class="title is-size-5 is-primary">Sour Tangie</p>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <p class="card-footer-item is-light">
-                    <a href={`/products/19`} class="is-primary">
-                      View
-                    </a>
-                  </p>
-                </footer>
-              </div>
-            </div>
-
-            <div class="column">
-              <div class="card has-background-primary">
-                <div class="card-image has-text-centered px-6">
-                  <img
-                    src="https://420spot.org/wp-content/uploads/2020/01/slurricane_2020_01.jpg"
-                    alt=" "
-                  />{" "}
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <p class="title is-size-5">Slurricane</p>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <p class="card-footer-item">
-                    <a href={`/products/21`} class="has-text-grey">
-                      View
-                    </a>
-                  </p>
-                </footer>
-              </div>
-            </div>
-
-            <div class="column">
-              <div class="card has-background-primary">
-                <div class="card-image has-text-centered px-6">
-                  <img
-                    src="https://firstclassmedicinal.com/wp-content/uploads/2020/02/Amnesia-Haze3.jpg"
-                    alt="Placeholder image"
-                  />
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <p class="title is-size-5">Amnesia Haze</p>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <div class="card-footer-item">
-                    <a href={`/products/17`} class="has-text-grey">
-                      View
-                    </a>
-                  </div>
-                </footer>
-              </div>
-            </div>
+            {sellerFeaturedProducts[randomNumbers[0]]}
+            {sellerFeaturedProducts[randomNumbers[1]]}
+            {sellerFeaturedProducts[randomNumbers[2]]}
           </div>
         </div>
 
